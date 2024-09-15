3a0eb635e1032caa70262f7c1d666039e4fefc54
Radha-Kishan-Sharma <erenjeager1b@gmail.com>
2024-09-15 09:54:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,68 +1,127 @@
-import speech_recognition as sr
-import webbrowser
 import pyttsx3
-import musicLibrary 
-import requests
+import speech_recognition as sr
+import keyboard
+import subprocess as sp
+import os
+import wolframalpha
 
+from decouple import config
+from conv import random_text
+from random import choice
+from online import search_on_wikipedia, search_on_google,youtube,get_news
 
-recognizer=sr.Recognizer()
-newsapi="5f90e0c2e10d4de0b8d285cac7a32ea5"
-engine = pyttsx3.init()
+engine = pyttsx3.init('sapi5')
+engine.setProperty('volume', 1.5)
+engine.setProperty('rate', 150)
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[1].id)
+
+USER=config('USER')
+HOST=config('BOT')
+
 def speak(text):
-    voices = engine.getProperty('voices')       #getting details of current voice
-    engine.setProperty('voice', voices[1].id)   #changing index, changes voices. o for male
     engine.say(text)
     engine.runAndWait()
 
+listening = False
 
-def processCommand(c):
-    if "open google" in c.lower():
-      webbrowser.open("https://google.com")
-    elif "open youtube" in c.lower():
-      webbrowser.open("https://youtube.com")  
-    elif c.lower().startswith("play"):
-       song=c.lower().split(" ")[1]
-       link=musicLibrary.music[song]
-       webbrowser.open(link)
 
-    elif "news" in c.lower():
-    
-         r=requests.get("https://newsapi.org/v2/top-headlines?country=in&category=sports&apiKey={newsapi}")
-      
-   
-         if r.status_code == 200:
-          data = r.json()
-          articles = data.get('articles',[])
-          for article in articles:
-            speak(article["title"])
-         else:
-          print(f"Error fetching articles: Status Code {r.status_code}")
+def start_listening():
+    global listening
+    listening = True
+    print("started listening ")
 
-if __name__=="__main__":
-    speak("Initializing Jarvis...")
 
-    # obtain audio from the microphone
+def pause_listening():
+    global listening
+    listening = False
+    print("stopped listening")
+
+
+keyboard.add_hotkey('s', start_listening)
+keyboard.add_hotkey('p', pause_listening)
+
+
+def greet_me():
+    speak(f"I am {HOST},How may I assist you? {USER}")
+
+def take_command():
+
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        r.pause_threshold = 1
+        audio = r.listen(source)
+
+    try:
+        print("Recognizing....")
+        queri = r.recognize_google(audio, language='en-in')
+        print(queri)
+        if not 'stop' in queri or 'exit' in queri:
+            speak(choice(random_text))
+        else:
+            speak("Have a good day sir!")
+            exit()
+
+    except Exception:
+        speak("Sorry I couldn't understand. Can you please repeat that?")
+        queri = 'None'
+    return queri
+
+
+if __name__ == '__main__':   
+    greet_me()
     while True:
-        r = sr.Recognizer()
-        
+        if listening:
+            query = take_command().lower()
+            if "how are you" in query:
+                speak("I am absolutely fine sir. What about you")
+                     
+            elif "open camera" in query:
+                speak("Opening camera sir")
+                sp.run('start microsoft.windows.camera:', shell=True)
+            
+            elif "open notepad" in query:
+                speak("Opening Notepad for you sir")
+                notepad_path = "C:\\Users\\radha\\AppData\\Local\\Microsoft\\WindowsApps\\notepad.exe"
+                os.startfile(notepad_path)
 
-        # recognize speech using google
-        try:
-         with sr.Microphone() as source:
-            print("Listening...")
-            audio = r.listen(source, timeout=2)
-            word=r.recognize_google(audio)
+            elif "open youtube" in query:
+                speak("What do you want to play on youtube sir?")
+                video = take_command().lower()
+                youtube(video)
 
-         if(word.lower()=="hey jarvis"):
-            speak("Yes Radha , what it is ?")
-         with sr.Microphone() as source:
-            print("Jarvis Active")
-            audio = r.listen(source, timeout=2)
-            command=r.recognize_google(audio)
+            elif "open google" in query:
+                speak(f"What do you want to search on google {USER}")
+                query = take_command().lower()
+                search_on_google(query)
 
-            processCommand(command)
+            elif "wikipedia" in query:
+                speak("what do you want to search on wikipedia sir?")
+                search = take_command().lower()
+                results = search_on_wikipedia(search)
+                speak(f"According to wikipedia,{results}")
+                speak("I am printing in on terminal")
+                print(results)  
+                search_on_wikipedia(search)
 
-        except Exception as e:
-         print("Error; {0}".format(e))
-      
-    
+            elif "give me news" in query:
+                speak(f"I am reading out the latest headline of today,sir")
+                speak(get_news())
+                speak("I am printing it on screen sir")
+                print(*get_news(), sep='\n')
+            
+            elif "calculate" in query:
+                app_id = "JJ6JKK-UK2JU5Y5L6"
+                client = wolframalpha.Client(app_id)
+                ind = query.lower().split().index("calculate")
+                text = query.split()[ind + 1:]
+                result = client.query(" ".join(text))
+                try:
+                    ans = next(result.results).text
+                    speak("The answer is " + ans)
+                    print("The answer is " + ans)
+                except StopIteration:
+                    speak("I couldn't find that . Please try again")
+
+           
